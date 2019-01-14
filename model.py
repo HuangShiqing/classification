2fe626ffc1dcea75f4a9595c5c103d41126cf35f
黄世青 <hsqyc@163.com>
2019-01-14 17:13:53 +0800

    adjusted the vgg16

--- model.py
+++ model.py
@@ -1,74 +1,48 @@
 from layer import *
 
 
-def vgg16_model(x, is_train=True):
+def vgg16_adjusted(x, is_train=True):
     n = 2
-    act = tf.nn.relu
-    n_filter = {'conv_11': 64, 'conv_12': 64, 'conv_21': 128, 'conv_22': 128, 'conv_31': 256, 'conv_32': 256,
-                'conv_33': 256, 'conv_41': 512, 'conv_42': 512, 'conv_43': 512,
-                'conv_51': 512, 'conv_52': 512, 'conv_53': 512}
 
-    net_in = InputLayer(x, name='input')
+    net = InputLayer(x, name='input')
     # conv1
-    net = Conv2d(net_in, n_filter['conv_11'], filter_size=(3, 3), strides=(1, 1), act=tf.nn.relu, padding='SAME',
-                 name='conv_11')
-    net = BatchNormLayer(net, epsilon=1e-3, act=act, is_train=is_train, name='bn_11')
-    net = Conv2d(net, n_filter=n_filter['conv_12'], filter_size=(3, 3), strides=(1, 1), act=tf.nn.relu, padding='SAME',
-                 name='conv_12')
-    net = BatchNormLayer(net, epsilon=1e-3, act=act, is_train=is_train, name='bn_12')
+    net = Conv_Bn(net, n_filter=64, filter_size=3, strides=1, act=tf.nn.relu, bn=True, is_train=is_train,
+                  padding='SAME', name='11')
+    net = Conv_Bn(net, n_filter=64, filter_size=3, strides=1, act=tf.nn.relu, bn=True, is_train=is_train,
+                  padding='SAME', name='12')
     net = MaxPool2d(net, filter_size=(2, 2), strides=(2, 2), padding='SAME', name='pool1')
-
     # conv2
-    net = Conv2d(net, n_filter=n_filter['conv_21'], filter_size=(3, 3), strides=(1, 1), act=tf.nn.relu, padding='SAME',
-                 name='conv_21')
-    net = BatchNormLayer(net, epsilon=1e-3, act=act, is_train=is_train, name='bn_21')
-    net = Conv2d(net, n_filter=n_filter['conv_22'], filter_size=(3, 3), strides=(1, 1), act=tf.nn.relu, padding='SAME',
-                 name='conv_22')
-    net = BatchNormLayer(net, epsilon=1e-3, act=act, is_train=is_train, name='bn_22')
+    net = Conv_Bn(net, n_filter=128, filter_size=3, strides=1, act=tf.nn.relu, bn=True, is_train=is_train,
+                  padding='SAME', name='21')
+    net = Conv_Bn(net, n_filter=128, filter_size=3, strides=1, act=tf.nn.relu, bn=True, is_train=is_train,
+                  padding='SAME', name='22')
     net = MaxPool2d(net, filter_size=(2, 2), strides=(2, 2), padding='SAME', name='pool2')
-
     # conv3
-    net = Conv2d(net, n_filter=n_filter['conv_31'], filter_size=(3, 3), strides=(1, 1), act=tf.nn.relu, padding='SAME',
-                 name='conv_31')
-    net = BatchNormLayer(net, epsilon=1e-3, act=act, is_train=is_train, name='bn_31')
-    net = Conv2d(net, n_filter=n_filter['conv_32'], filter_size=(3, 3), strides=(1, 1), act=tf.nn.relu, padding='SAME',
-                 name='conv_32')
-    net = BatchNormLayer(net, epsilon=1e-3, act=act, is_train=is_train, name='bn_32')
-    net = Conv2d(net, n_filter=n_filter['conv_33'], filter_size=(3, 3), strides=(1, 1), act=tf.nn.relu, padding='SAME',
-                 name='conv_33')
-    net = BatchNormLayer(net, epsilon=1e-3, act=act, is_train=is_train, name='bn_33')
+    net = Conv_Bn(net, n_filter=256, filter_size=3, strides=1, act=tf.nn.relu, bn=True, is_train=is_train,
+                  padding='SAME', name='31')
+    net = Conv_Bn(net, n_filter=256, filter_size=3, strides=1, act=tf.nn.relu, bn=True, is_train=is_train,
+                  padding='SAME', name='32')
+    net = Conv_Bn(net, n_filter=256, filter_size=3, strides=1, act=tf.nn.relu, bn=True, is_train=is_train,
+                  padding='SAME', name='33')
     net = MaxPool2d(net, filter_size=(2, 2), strides=(2, 2), padding='SAME', name='pool3')
-
     # conv4
-    net = Conv2d(net, n_filter=n_filter['conv_41'], filter_size=(3, 3), strides=(1, 1), act=tf.nn.relu, padding='SAME',
-                 name='conv_41')
-    net = BatchNormLayer(net, epsilon=1e-3, act=act, is_train=is_train, name='bn_41')
-    net = Conv2d(net, n_filter=n_filter['conv_42'], filter_size=(3, 3), strides=(1, 1), act=tf.nn.relu, padding='SAME',
-                 name='conv_42')
-    net = BatchNormLayer(net, epsilon=1e-3, act=act, is_train=is_train, name='bn_42')
-    net = Conv2d(net, n_filter=n_filter['conv_43'], filter_size=(3, 3), strides=(1, 1), act=tf.nn.relu, padding='SAME',
-                 name='conv_43')
-    net = BatchNormLayer(net, epsilon=1e-3, act=act, is_train=is_train, name='bn_43')
+    net = Conv_Bn(net, n_filter=512, filter_size=3, strides=1, act=tf.nn.relu, bn=True, is_train=is_train,
+                  padding='SAME', name='41')
+    net = Conv_Bn(net, n_filter=512, filter_size=3, strides=1, act=tf.nn.relu, bn=True, is_train=is_train,
+                  padding='SAME', name='42')
+    net = Conv_Bn(net, n_filter=512, filter_size=3, strides=1, act=tf.nn.relu, bn=True, is_train=is_train,
+                  padding='SAME', name='43')
     net = MaxPool2d(net, filter_size=(2, 2), strides=(2, 2), padding='SAME', name='pool4')
-
     # conv5
-    net = Conv2d(net, n_filter=n_filter['conv_51'], filter_size=(3, 3), strides=(1, 1), act=tf.nn.relu, padding='SAME',
-                 name='conv_51')
-    net = BatchNormLayer(net, epsilon=1e-3, act=act, is_train=is_train, name='bn_51')
-    net = Conv2d(net, n_filter=n_filter['conv_52'], filter_size=(3, 3), strides=(1, 1), act=tf.nn.relu, padding='SAME',
-                 name='conv_52')
-    net = BatchNormLayer(net, epsilon=1e-3, act=act, is_train=is_train, name='bn_52')
-    net = Conv2d(net, n_filter=n_filter['conv_53'], filter_size=(3, 3), strides=(1, 1), act=tf.nn.relu, padding='SAME',
-                 name='conv_53')
-    net = BatchNormLayer(net, epsilon=1e-3, act=act, is_train=is_train, name='bn_53')
-
-    # net = FlattenLayer(net, name='flatten')
-    # net = DenseLayer(net, n_units=4096, act=tf.nn.relu, name='fc1_relu')
-    # net = DenseLayer(net, n_units=4096, act=tf.nn.relu, name='fc2_relu')
-    # net = DenseLayer(net, n_units=n, act=None, name='fc3_relu')
+    net = Conv_Bn(net, n_filter=512, filter_size=3, strides=1, act=tf.nn.relu, bn=True, is_train=is_train,
+                  padding='SAME', name='51')
+    net = Conv_Bn(net, n_filter=512, filter_size=3, strides=1, act=tf.nn.relu, bn=True, is_train=is_train,
+                  padding='SAME', name='52')
+    net = Conv_Bn(net, n_filter=512, filter_size=3, strides=1, act=tf.nn.relu, bn=True, is_train=is_train,
+                  padding='SAME', name='53')
 
     net = GlobalMeanPool2d(net)
-    net = DenseLayer(net, n_units=n, act=None, name='fc3_identity')
+    net = DenseLayer(net, n_units=n, act=None, name='dense')
     return net.outputs, net
 
 
